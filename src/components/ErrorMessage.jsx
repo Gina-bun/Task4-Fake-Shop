e37d9fda6948691ce5d99e96c55fe23e9c42b8a3
Gina-bun <64642337+Gina-bun@users.noreply.github.com>
2026-02-13 15:15:06 +0000

    style: add width for error message mobile view

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -5,7 +5,7 @@ export function ErrorMessage({error}){
         <>
         <div className="error-container h-screen flex flex-col items-center justify-center">
             <img className="error-image w-[10em]" src={ErrorImg} alt="Error" />
-            <h2>{error}</h2>
+            <h2 className="max-sm:w-[70%]">{error}</h2>
         </div>
         </>
     )
